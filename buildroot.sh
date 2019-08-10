28a8f06d0afd1bd432d8b90ff1548ac4ea33067a
MitchRatquest <mitchell.dokken@gmail.com>
2019-08-10 15:39:34 -0500

    Display a help message I wish I'd seen 10 years ago

--- buildroot.sh
+++ buildroot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-EXTRA_PACKAGES=(gcc bash)
-TOPDIR=`pwd`
+#space separated package names will be installed via static-get
+#for example: EXTRA_PACKAGES=(gcc bash gawk)
+EXTRA_PACKAGES=()
 
 function main() {
     get_buildroot
@@ -9,6 +10,7 @@ function main() {
     populate_overlay
     install_extras
     cd "$TOPDIR" && make -j$(nproc)
+    display_message
     boot_image
 }
 
@@ -75,10 +77,68 @@ function install_extras() {
     fi
 }
 
+function display_message() {
+    color_print green bold 'Thanks for trying this out!
+By default, this has made a very minimal image using buildroot
+'
+
+    color_print green bold 'If you want to change any settings in buildroot
+such as packages, build systems, filesystem types, etc, type'; color_print normal inv 'make menuconfig'
+    color_print green bold 'If you want to change kernel settings, type'; color_print normal inv 'make linux-menuconfig'
+    color_print green bold 'To change the busybox config, type'; color_print normal inv  'make busybox-menuconfig'
+    color_print green bold 'If you use uboot, type'; color_print normal inv 'make uboot-menuconfig'
+    color_print green bold 'Ff you use uClibc, type'; color_print normal inv 'make uclibc-menuconfig'
+    echo -ne '\n\n\n'
+    color_print green bold 'I wish someone had told these things when starting linux devops:
+/ in menuconfig enables a search function
+and that you can jump to any options by 1-9, with the top search return being 1
+
+pressing space on an option enables/disables it
+* means its built in
+m means it will be built as a module
+h will show some helpful information
+
+the highlighted letter at in each option is a hotkey to jump to it
+by pressing that letter
+for example (each new line means "press enter"):
+make linux-menuconfig
+d
+sssssss
+
+will get you into the sound card support device drivers menu
+
+you could also type (each new line means "press enter"):
+/
+sound
+1
+to get there too!'
+}
 
 function boot_image() {
     cd "$TOPDIR"
-    qemu-system-x86_64 -m 512M -cdrom images/rootfs.iso9660 -boot d -vga std -smp 4 -device e1000
+    cp images/rootfs.iso9660 br-rootfs.iso
+    qemu-system-x86_64 -m 512M -cdrom br-rootfs.iso -boot d -vga std -smp 4 -device e1000
 }
 
+function color_print() {
+    case "$1" in
+        red*)       color='\033[0;31m'  ;;
+        orange*)    color='\033[0;33m'  ;;
+        green*)     color='\033[0;32m'  ;;
+        cyan*)      color='\e[96m'      ;;
+        *)          color='\033[0m\e[0m';;
+    esac
+    nocolor='\033[0m\e[0m\e[0m'
+    shift
+    case "$1" in
+        ul*)    modifier='\e[4m'    ;;
+        inv*)   modifier='\e[7m'    ;;
+        bold*)  modifier='\e[1m'    ;;
+        *)      modifier='\e[0m'    ;;
+    esac
+    shift
+    printf "${color}${modifier}$@${nocolor}\n"
+}
+
+TOPDIR=`pwd`
 main "$@"
