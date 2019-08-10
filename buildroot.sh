a694498e8ff63299b54eeaba42bb1c70dfe29e8b
MitchRatquest <mitchell.dokken@gmail.com>
2019-08-10 15:00:41 -0500

    External toolchains dont support sysroot, breaking compatibility with buildroot

--- buildroot.sh
+++ buildroot.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 EXTRA_PACKAGES=(gcc bash)
-USE_EXTERNAL_TOOLCHAIN= #can be 'no' for buildroot, or 'musl', or 'gcc'
+
 
 function main() {
     get_buildroot
     set_configurations
-    get_external_toolchain
     create_overlay
     populate_overlay
     install_extras
@@ -21,35 +19,6 @@ function get_buildroot() {
         cd buildroot
         git checkout "$commit"
         cd ..
-    fi
-}
-
-function get_external_toolchain() {
-    case "$USE_EXTERNAL_TOOLCHAIN" in
-        gcc*) get_and_configure_toolchain gcc   ;;
-        musl*) get_and_configure_toolchain musl ;;
-        no*)  echo -ne                          ;;
-    esac
-}
-
-function get_and_configure_toolchain() {
-    #needs to be >= 4.6.0 for linux >= 4.19 
-    #https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cafa0010cd51fb711fdcb50fc55f394c5f167a0a
-    toolchain="$1"
-    if [ ! -f static-get ]; then
-        wget https://raw.githubusercontent.com/minos-org/minos-static/master/static-get
-        chmod 777 static-get
-    fi
-    mkdir external_toolchain
-    cd external_toolchain
-    ../static-get -x "$1"
-#• Use a completely custom external toolchain. This is particularly useful for toolchains generated using crosstool-NG or with
-#Buildroot itself. To do this, select the Custom toolchain solution in the Toolchain list. You need to fill the Toolch
-#ain path, Toolchain prefix and External toolchain C library options. Then, you have to tell Buildroot
-#what your external toolchain supports. If your external toolchain uses the glibc library, you only have to tell whether your
-#toolchain supports C++ or not and whether it has built-in RPC support. If your external toolchain uses the uClibc library, then
-#you have to tell Buildroot if it supports RPC, wide-char, locale, program invocation, threads and C++. At the beginning of the
-#execution, Buildroot will tell you if the selected options do not match the toolchain configuration.
     fi
 }
 
