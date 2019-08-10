0b5b4edfbdc2853c947ea9c532c275645474eaa7
MitchRatquest <mitchell.dokken@gmail.com>
2019-08-10 13:58:28 -0500

    Setup dvorak in overlay

--- buildroot.sh
+++ buildroot.sh
@@ -3,6 +3,8 @@
 function main() {
 	get_buildroot
 	set_configurations
+	create_overlay
+	populate_overlay
 
 }
 
@@ -16,4 +18,21 @@ function get_buildroot() {
 
 function set_configurations() {
 	make O=$PWD buildroot/ defconfig BR2_DEFCONFIG=br_defconfig
+}
+
+function create_ovelay() {
+	mkdir -p overlay
+}
+
+function populate_overlay() {
+	mkdir -p overlay/usr/share/kmaps
+	mkdir -p overlay/etc/init.d
+	cp dvorak.kmap overlay/usr/share/kmaps/dvorak.kmap
+	echo -ne > overlay/etc/init.d/S99-dvorak
+	cat >> overlay/etc/init.d/S99-dvorak << EOF
+#!/bin/sh
+loadkmap < /usr/share/kmaps/dvorak.kmap
+exit 0
+EOF
+	chmod 777 overlay/etc/init.d/S99-dvorak
 }
